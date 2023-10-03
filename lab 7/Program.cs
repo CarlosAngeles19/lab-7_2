cf6ec2044bef22830c3404ab658f94ca0d250388
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-10-03 16:41:38 -0500

    Agregar archivos de proyecto.

---
+++ lab 7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Divisores de Omar Orlandini:");
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Divisor de Omar Orlandini " + (i / 3) + ": " + i);
+            }
+        }
+    }
+}
